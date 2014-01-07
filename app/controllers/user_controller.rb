114083bbc9f55099bd4730a77b5d9bb22ed9631a
lizhim <dachuan212@163.com>
2014-01-07 20:11:01 +0800

    判断新用户注册时填写的注册信息是否完整

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,6 +2,21 @@ class UserController < ApplicationController
   def register
 
   end
+  def information_complete
+    @user_name= params[:user][:name]
+    @user_password = params[:user][:password]
+    @password_confirm = params[:user][:password_confirm]
+    @question = params[:user][:question]
+    @answer = params[:user][:answer]
+    if @user_name!=''&&@user_password!=''&&@password_confirm!=''&&@question!=''&&@answer!=''
+      return judge_user_account
+    else
+      flash[:notice] = "请将注册信息填写完整"
+      render '/user/register'
+    end
+  end
+
+
 
   def user_params
     params.require(:user).permit(:name, :password, :password_confirm, :question, :answer, :admin)
