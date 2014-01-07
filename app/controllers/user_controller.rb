6bfeb2543ce18acacc4f686d3f26564d6e8f7e16
lizhim <dachuan212@163.com>
2014-01-07 20:08:00 +0800

    用户注册界面完成

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,1 +1,9 @@
+class UserController < ApplicationController
+  def register
 
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirm, :question, :answer, :admin)
+  end
+end
