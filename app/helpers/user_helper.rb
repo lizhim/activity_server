43e9ab757a01ce4c75cf95599e96180636edcf74
lizhim <dachuan212@163.com>
2014-01-27 14:11:36 +0800

    把haml中的的代码放到helper中

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,18 @@
 module UserHelper
+
+  def bid_detail
+    if @bid_winner.nil?
+      return content_tag(:h3, "活动正在进行中...")
+    end
+    if !@bid_winner.nil?&&@bid_winner["name"]==''
+      return content_tag(:h3, "本次竞价无人胜出")
+    end
+    if !@bid_winner.nil?&&@bid_winner["name"]!=''
+      name='获胜者:'+@bid_winner["name"]
+      content_tag(:br)
+      price='出价:'+@bid_winner["price"]+'元'
+      phone='手机号:'+@bid_winner["phone"]
+      content_tag(:h3, name + price + phone)
+    end
+  end
 end
